b3453cf039214c16d7d007818fd17e69e1b39b2b
TonnyBashizi <tonnybash5@gmail.com>
2020-12-06 10:15:20 +0100

    newAndroidxDesign

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,414 @@
 import 'package:flutter/material.dart';
+import 'package:MagClient/CustomAppBar.dart';
+import 'package:MagClient/CustomShapeClipper.dart';
+import 'package:MagClient/NotificationPlugin.dart';
+import 'package:MagClient/expedier.dart';
+import 'package:MagClient/flight_list.dart';
+import 'package:MagClient/relais_list.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MaterialApp(
+      title: 'Tracking bagages',
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+      theme: appTheme,
+    ));
+Color firstColor = Colors.green;
+Color secondColor = Colors.white;
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+// Color firstColor = Color(0xFFFF0000);
+// Color secondColor = Color(0xFFEF772C);
+
+ThemeData appTheme = ThemeData(
+  primaryColor: Color(0xFFF3791A),
+);
+
+List<String> locations = ['DRC', 'MAGTECH'];
+const TextStyle dropDownLabelStyle =
+    TextStyle(color: Colors.black, fontSize: 16.0);
+const TextStyle dropDownMenuItemStyle =
+    TextStyle(color: Colors.black, fontSize: 16.0);
+
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return Scaffold(
+      bottomNavigationBar: CustomAppBar(),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: <Widget>[
+            HomeScreenTopPart(),
+            homeScreenBottomPart,
+            homeScreenBottomPart,
+          ],
+        ),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class HomeScreenTopPart extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomeScreenTopPartState createState() => _HomeScreenTopPartState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomeScreenTopPartState extends State<HomeScreenTopPart> {
+  var selectedLocationIndex = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    // notificationPlugin.setListenerLowerVersions(onNotificationInLowerVersions);
+    // notificationPlugin.setOnNotificationClick(onNotificationClick);
   }
+
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        ClipPath(
+          clipper: CustomShapeClipper(),
+          child: Container(
+            height: 400.0,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [firstColor, secondColor],
+              ),
+            ),
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: 50.0),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(Icons.location_on, color: Colors.white),
+                      SizedBox(
+                        width: 16.0,
+                      ),
+                      PopupMenuButton(
+                        onSelected: (index) {
+                          setState(() {
+                            selectedLocationIndex = index;
+                          });
+                        },
+                        child: Row(
+                          children: <Widget>[
+                            Text(
+                              locations[selectedLocationIndex],
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            Icon(
+                              Icons.keyboard_arrow_down,
+                              color: Colors.black,
+                            )
+                          ],
+                        ),
+                        itemBuilder: (BuildContext context) =>
+                            <PopupMenuItem<int>>[
+                          PopupMenuItem(
+                            child: Text(
+                              locations[0],
+                              style: dropDownMenuItemStyle,
+                            ),
+                            value: 0,
+                          ),
+                          PopupMenuItem(
+                            child: Text(
+                              locations[1],
+                              style: dropDownMenuItemStyle,
+                            ),
+                            value: 1,
+                          ),
+                        ],
+                      ),
+                      Spacer(),
+                      Icon(
+                        Icons.settings,
+                        color: Colors.black,
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 50.0,
+                ),
+                Text(
+                  'MagTech\n Soft ',
+                  style: TextStyle(
+                    fontSize: 24.0,
+                    color: Colors.white,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 32.0),
+                  child: Material(
+                    elevation: 5.0,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(30.0),
+                    ),
+                    child: TextField(
+                      controller: TextEditingController(
+                        text: "",
+                      ),
+                      style: dropDownMenuItemStyle,
+                      cursorColor: appTheme.primaryColor,
+                      decoration: InputDecoration(
+                        contentPadding: EdgeInsets.symmetric(
+                            horizontal: 16.0, vertical: 14.0),
+                        suffixIcon: Material(
+                          elevation: 2.0,
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(30.0),
+                          ),
+                          child: InkWell(
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          FlightListingScreen()));
+                            },
+                            child: Icon(
+                              Icons.credit_card,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    ChoiceChip(Icons.business, "Station and Business"),
+                    SizedBox(
+                      width: 30.0,
+                    ),
+                    InkWell(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => expedier()));
+                      },
+                      child: ChoiceChip(Icons.send, "Transfert"),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class ChoiceChip extends StatefulWidget {
+  final IconData icon;
+  final String text;
+  ChoiceChip(this.icon, this.text);
+
+  @override
+  _ChoiceChipState createState() => _ChoiceChipState();
+}
+
+class _ChoiceChipState extends State<ChoiceChip> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Icon(
+          widget.icon,
+          size: 20.0,
+          color: Colors.white,
+        ),
+        SizedBox(
+          width: 8.0,
+        ),
+        Text(widget.text,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 14.0,
+            ))
+      ],
+    );
+  }
+}
+
+var viewAllStyle = TextStyle(fontSize: 14.0, color: appTheme.primaryColor);
+var homeScreenBottomPart = Column(
+  children: <Widget>[
+    Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          SizedBox(
+            width: 10.0,
+          ),
+          Text(
+            "Partenaires MagTech",
+            style: dropDownMenuItemStyle,
+          ),
+          Spacer(),
+          Text(
+            "+",
+            style: viewAllStyle,
+          ),
+        ],
+      ),
+    ),
+    Container(
+      height: 210.0,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: cityCards,
+      ),
+    )
+  ],
+);
+
+List<CityCard> cityCards = [
+  CityCard("assets/images/cobil.jpeg", "RDC Congo", "Katindo", "Goma",
+      "Station de service", "80.0"),
+  CityCard("assets/images/takenga.jpeg", "RDC Congo", "Centre", "Butembo",
+      "Station de service", "10"),
+  CityCard("assets/images/carwash.jpg", "RDC Congo", "Goma", "BDGL",
+      "Station de service", "80"),
+];
+
+// final formatCurrency = NumberFormat.s.
+
+class CityCard extends StatelessWidget {
+  final String imagePath, cityname, monthYear, discount, oldprice, newprice;
+
+  CityCard(this.imagePath, this.monthYear, this.discount, this.oldprice,
+      this.newprice, this.cityname);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: Column(
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.all(
+              Radius.circular(20.0),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            child: InkWell(
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => relaisList()));
+              },
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    height: 150.0,
+                    width: 160.0,
+                    child: Image.asset(
+                      imagePath,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Positioned(
+                    left: 0.0,
+                    bottom: 0.0,
+                    width: 160.0,
+                    height: 60.0,
+                    child: Container(
+                      decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                              begin: Alignment.bottomCenter,
+                              end: Alignment.topCenter,
+                              colors: [
+                            Colors.black,
+                            Colors.black.withOpacity(0.1),
+                          ])),
+                    ),
+                  ),
+                  Positioned(
+                    left: 10.0,
+                    bottom: 10.0,
+                    right: 5.0,
+                    child: Row(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(oldprice,
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                    fontSize: 18.0)),
+                            Text(monthYear,
+                                style: TextStyle(
+                                    fontWeight: FontWeight.normal,
+                                    color: Colors.white,
+                                    fontSize: 14.0)),
+                          ],
+                        ),
+                        Container(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 6.0, vertical: 2.0),
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              shape: BoxShape.rectangle,
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(10.0),
+                              )),
+                          child: Text(
+                            "$discount%",
+                            style:
+                                TextStyle(fontSize: 10.0, color: Colors.black),
+                          ),
+                        ),
+                        // InkWell(
+                        //    onTap: (){
+                        //                 Navigator.push(context, MaterialPageRoute(builder: (context) => relaisList()));
+                        //            },
+                        // ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
-          ],
-        ),
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(
+                width: 5.0,
+              ),
+              Text(newprice,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  )),
+              SizedBox(
+                width: 5.0,
+              ),
+            ],
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
---
+++ lib/utils/config.dart
@@ -0,0 +1,8 @@
+class BaseUrl {
+  static String ip="http://192.168.43.141/Projects/MagCl/Api/read.php";
+  static String getColi=ip+"getColi.php?value=listColi";
+  static String img="http://192.168.43.141/Projects/MagCl/Api/images/";
+
+
+  // static String getColiByID=ip+"getColi.php?value=listColi";
+}
